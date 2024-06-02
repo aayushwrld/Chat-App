52ad1142870494a4054630ad75633596f50d84bd
aayushwrld <aayush.arora@kalvium.community>
2024-06-02 14:42:06 +0530

    Backend Done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -11,12 +14,15 @@ dotenv.config()
 const port = process.env.PORT || 8080
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res)=>{
     res.json("Backend Route Working ✅")
 })
 
 app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(port, ()=>{
     connectToMongoDB()
