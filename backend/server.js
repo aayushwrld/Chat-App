70c55720e5bbbe16ac3b8caa7f2757ef5ee5fe2b
aayushwrld <aayush.arora@kalvium.community>
2024-06-10 01:47:01 +0530

    Backend Bug Fix

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,31 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
-import authRoutes from './routes/auth.routes.js'
-import messageRoutes from './routes/message.routes.js'
-import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
 
-dotenv.config()
+dotenv.config({ path: path.resolve('../.env') });
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res)=>{
-    res.json("Backend Route Working ✅")
+    res.json("Backend Route Working ✅");
 })
 
-app.use('/api/auth', authRoutes)
-app.use('/api/messages', messageRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(port, ()=>{
-    connectToMongoDB()
+    connectToMongoDB();
     console.log(`Listening to ${port} 🚀!`);
 })
